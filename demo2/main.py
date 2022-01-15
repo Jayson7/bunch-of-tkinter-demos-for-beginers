dc31694708e78ad2b9ef98b0aa79dc9295ae7901
jayson7 <lexxiijoo70@gmail.com>
2022-01-15 01:51:19 +0100

    demo2

--- demo2/main.py
+++ demo2/main.py
@@ -3,8 +3,11 @@ from tkinter import *
 # initialize tk
 root = Tk()
 
+# a simple hello world message 
 my_label = Label(root, text="Hello world jayson")
 
+# send the label to the tk
 my_label.pack()
 
+# intiate the loop
 root.mainloop()
