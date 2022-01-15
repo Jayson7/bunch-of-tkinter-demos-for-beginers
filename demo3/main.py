6a72260cd0f248e9ac4d2f3f1eeb3b21e154d794
jayson7 <lexxiijoo70@gmail.com>
2022-01-15 02:13:43 +0100

    demo2

--- demo3/main.py
+++ demo3/main.py
@@ -5,13 +5,15 @@ root = Tk()
 
 # a simple hello world message 
 my_label = Label(root, text="Hello world jayson")
+my_label2 = Label(root, text="Hello world jayson 2")
 
 # send the label to the tk
 my_label.pack()
 
 # positioning 
 
-my_label.grid(row=2, column=5)
+my_label.grid(row=1, column=1)
+my_label2.grid(row=2, column=0)
 
 
 # intiate the loop
