b171e70f33b0d288292587d2b590f043331f9fa2
jayson7 <lexxiijoo70@gmail.com>
2022-01-16 23:34:30 +0100

    tkinter

---
+++ demo5(buttons)/buttonWithPadY.py
@@ -0,0 +1,12 @@
+from tkinter import *
+root = Tk()
+
+myButton = Button(root, text="Click", pady=50)
+
+myButton.pack()
+
+
+
+
+
+root.mainloop() 
---
+++ demo5(buttons)/disabled.py
@@ -0,0 +1,12 @@
+from tkinter import *
+root = Tk()
+
+myButton = Button(root, text="Click", state=DISABLED)
+
+myButton.pack()
+
+
+
+
+
+root.mainloop() 
