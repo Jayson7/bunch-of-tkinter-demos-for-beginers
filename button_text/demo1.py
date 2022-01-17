eeb3bdaa55d497eb5b338ae42ce20ff63d204851
jayson7 <lexxiijoo70@gmail.com>
2022-01-17 08:57:08 +0100

    tkinter

--- button_text/demo1.py
+++ button_text/demo1.py
@@ -2,8 +2,12 @@ from tkinter import *
 
 root = Tk()
 
+def text_respond():
+    text_res = Label(root, text="button triggered!")
+    text_res.pack()
+    
 
-button = Button(root, text="click me", padx=20)
+button = Button(root, text="click me", padx=20, command=text_respond, fg="grey", bg="#445" )
 
 button.pack()
 
