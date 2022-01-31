45f00c78058520c566d3f6a637144db0af79e5cc
jayson7 <lexxiijoo70@gmail.com>
2022-01-31 11:51:56 +0100

    calculator

--- calculator/main.py
+++ calculator/main.py
@@ -14,6 +14,15 @@ def button_click(number):
 def button_clear():
     entry.delete(0, END)
 
+
+def button_add():
+    first_number = entry.get()
+    global f_num
+    f_num = int(first_number)
+    entry.delete(0, END)
+    
+
+
 # all buttons needed
 
 button_1 = Button(root, text="1", padx=25, pady=10, command=lambda: button_click(1) )
@@ -26,7 +35,7 @@ button_7 = Button(root, text="7", padx=25, pady=10, command=lambda: button_click
 button_8 = Button(root, text="8", padx=25, pady=10, command=lambda: button_click(8) )
 button_9 = Button(root, text="9", padx=25, pady=10, command=lambda: button_click(9) )
 button_0 = Button(root, text="0", padx=25, pady=10, command=lambda: button_click(0) )
-button_add = Button(root, text="+", padx=25, pady=20, command=lambda: button_click() )
+button_add = Button(root, text="+", padx=25, pady=20, command=button_add )
 button_equal = Button(root, text="=", padx=60, pady=20, command=lambda: button_click() )
 button_clear = Button(root, text="Clear", padx=50, pady=10, command=button_clear )
 
