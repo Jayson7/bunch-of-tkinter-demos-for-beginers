de75c225d78f1fed22be4233fecfb76fc8373e18
jayson7 <lexxiijoo70@gmail.com>
2022-02-01 09:49:21 +0100

    images

---
+++ images & other demo/1.py
@@ -0,0 +1,30 @@
+from  tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+
+root.title("Jayson - software developer")
+
+# favicon
+root.iconbitmap("4.jpg")
+
+my_img = ImageTk.PhotoImage(Image.open('4.jpg'))
+
+
+
+
+
+
+
+
+
+
+
+# exit button
+button_quit = Button(root, text="Exit program", command=root.quit)
+button_quit.pack()
+
+
+
+
+root.mainloop()
