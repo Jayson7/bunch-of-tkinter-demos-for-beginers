4ac3d70356646b23d6eb1d49b446fedea1df9fd5
jayson7 <lexxiijoo70@gmail.com>
2022-02-01 10:44:51 +0100

    images

--- images & other demo/1.py
+++ images & other demo/1.py
@@ -4,12 +4,13 @@ from PIL import ImageTk, Image
 root = Tk()
 
 root.title("Jayson - software developer")
-
+root.geometry("750x600")
 # favicon
 # root.iconbitmap("/home/favicon.ico")
 
 my_img = ImageTk.PhotoImage(Image.open('./4.jpg'))
-my_label = Label(image=my_img, width=300, height=300)
+my_label = Label(image=my_img  )
+my_label.grid(row=0, column=0, columnspan=3)
 # print(dir(my_label))
 my_label.pack()
 
